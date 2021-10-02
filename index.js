165992c7f59870b6092f1aba77e9659f665e3560
denhamdagger <denhamdagger@gmail.com>
2021-10-02 15:58:23 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+console.log("::group::Environment variables")
+for (const[key, value] of Object.entries(process.env).sort()) {
+  console.log('${key} = ${value}');
+}
+console.log("::endgroup::");
+
+console.log("::group::Event JSON");
+const event = JSON.parse(fs.readFileSync(process.env["GITHUB_EVENT_PATH"]));
+console.log(JSON.stringify(event, null, 2));
+console.log("::endgroup::");
